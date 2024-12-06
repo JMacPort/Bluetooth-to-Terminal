4ddd06e81409ba97649bcadba089cccff86f308f
jmac1 <145376972+JMacPort@users.noreply.github.com>
2024-12-06 12:38:38 -0500

    Fully implemented. Basic command handling, interrupt triggering and circular buffer to store command strings

--- app.py
+++ app.py
@@ -5,7 +5,29 @@ ser = serial.Serial('COM4', 9600, timeout=1)
 print("Port opened successfully")
 
 while True:
-    if ser.in_waiting > 0:
-        data = ser.read(ser.in_waiting)
-        print(data.decode(), end='', flush=True)
-    time.sleep(0.1)
+    cmd = input("Enter command: ")
+    if cmd.lower() == 'quit':
+        break
+
+    full_cmd = cmd + '\r\n'
+    ser.write(full_cmd.encode())
+    ser.flush()
+
+    time.sleep(0.2)
+    response = ""
+
+    while True:
+        if ser.in_waiting > 0:
+            response += ser.read(ser.in_waiting).decode()
+            if '\n' in response:
+                break
+        time.sleep(0.1)
+
+    if response:
+        print(response)
+    else:
+        print("No response received")
+
+    ser.reset_input_buffer()
+
+ser.close()
